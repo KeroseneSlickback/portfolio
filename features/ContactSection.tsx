0844107d1115f470e25cd70b74053f07ecb942aa
Will <mitchellspaur@gmail.com>
2022-09-28 19:36:32 +0900

    small change to contact styling

--- features/ContactSection.tsx
+++ features/ContactSection.tsx
@@ -54,6 +54,7 @@ const ContactWrapper = styled.div`
     flex-direction: column;
     align-items: center;
     justify-content: center;
+    padding: 0.75rem;
     color: ${({ theme }) => theme.colors.textPrimary};
     div {
       svg {
